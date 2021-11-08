ff72f9ccb8919a64210d5922abf0a9cd922aedb2
eithri <guillermo.paez@uptc.edu.co>
2021-11-08 16:01:35 -0500

    added react-bootstrap

---
+++ src/fontawesome.js
@@ -0,0 +1,7 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+// import your icons
+
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faExternalLinkAlt);
