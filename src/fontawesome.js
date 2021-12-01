70072e3a170fa36256649666ef1c60f84d740664
eithri <guillermo.paez@uptc.edu.co>
2021-12-01 15:08:55 -0500

    added jumbotron with icons

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -2,6 +2,11 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 
 // import your icons
 
+import {
+  faTiktok,
+  faFacebook,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faExternalLinkAlt);
+library.add(faExternalLinkAlt, faTiktok, faFacebook, faInstagram);
