c7c03176fe35f966413a66b2c16e0503208ffbce
CinthyaR31 <cinthyarojas2@gmail.com>
2021-12-04 17:59:43 -0500

    add module atuh

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import "./App.css";
 
 //Components
 import Home from "./pages/home";
+import axios from "axios";
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+
+require('dotenv').config()
+
+
 
 function App() {
   return (
