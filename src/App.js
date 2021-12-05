8b14a5a7f8cb1e409039464fcfa1c1ed89ecdde7
eithri <guillermo.paez@uptc.edu.co>
2021-12-05 00:40:42 -0500

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "./App.css";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 //Components
 import Home from "./pages/home";
+import Error404 from "./pages/error404";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          <DashboardMenu />
+        </Route>
+        <Route path="/" exact component={Home} />
+        <Route path="*" exact component={Error404} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/error404/error404.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import background from "../../assets/images/background404.png";
+
+function Error404() {
+  return (
+    <div style={{ backgroundImage: `url(${background})` }}>
+      Hello World
+    </div>
+  );
+}
+
+export default Error404;
